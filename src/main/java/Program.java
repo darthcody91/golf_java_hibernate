fdf0d65a491b32103df997412788c20dc29968c2
darthcody91 <45984035+darthcody91@users.noreply.github.com>
2023-03-04 21:31:17 -0700

    Added user input

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.Scanner;
+
 
 public class Program {
     public static void main(String[] args) {
@@ -11,20 +13,43 @@ public class Program {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
 
+        //Add scanner to get a user input of the strokes//
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Hole 1: ");
+        int userHole1 = userInput.nextInt();
+        System.out.println("Hole 2: ");
+        int userHole2 = userInput.nextInt();
+        System.out.println("Hole 3: ");
+        int userHole3 = userInput.nextInt();
+        System.out.println("Hole 4: ");
+        int userHole4 = userInput.nextInt();
+        System.out.println("Hole 5: ");
+        int userHole5 = userInput.nextInt();
+        System.out.println("Hole 6: ");
+        int userHole6 = userInput.nextInt();
+        System.out.println("Hole 7: ");
+        int userHole7 = userInput.nextInt();
+        System.out.println("Hole 8: ");
+        int userHole8 = userInput.nextInt();
+        System.out.println("Hole 9: ");
+        int userHole9 = userInput.nextInt();
+
+
         try {
             transaction.begin();
 
             Skyway stroke = new Skyway();
             //stroke.setRound(2);//
-            stroke.setHole1(4);
-            stroke.setHole2(4);
-            stroke.setHole3(4);
-            stroke.setHole4(4);
-            stroke.setHole5(5);
-            stroke.setHole6(3);
-            stroke.setHole7(5);
-            stroke.setHole8(3);
-            stroke.setHole9(4);
+            stroke.setHole1(userHole1);
+            stroke.setHole2(userHole2);
+            stroke.setHole3(userHole3);
+            stroke.setHole4(userHole4);
+            stroke.setHole5(userHole5);
+            stroke.setHole6(userHole6);
+            stroke.setHole7(userHole7);
+            stroke.setHole8(userHole8);
+            stroke.setHole9(userHole9);
             entityManager.persist(stroke);
 
             transaction.commit();
